eb4f549d7c73fb75b8cff18a9580340e0bbcf92c
Syd Shields <sshields750@gmail.com>
2022-01-23 01:02:18 -0500

    no change

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
 import Head from "next/head";
+import styles from '../styles/Home.module.scss'
+import Navbar from "../components/navbar/Navbar";
 
 export default function Home() {
   return (
     <div>
-    <Head>
-      <title>Analogic | Gear made easy</title>
-      <meta
-        name="description"
-        content="something about the site here"
-      />
-    </Head>
-      <h1>Analogic</h1>
+      <Head>
+        <title>Analogic | Gear made easy</title>
+        <meta name="description" content="something about the site here" />
+      </Head>
+      <div className={styles.homepage}>
+        <Navbar />
+      </div>
     </div>
-  )
+  );
 }
