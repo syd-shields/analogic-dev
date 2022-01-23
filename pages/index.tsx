97ec413c524f43764599e5b3d45f4f5a46072372
Syd Shields <sshields750@gmail.com>
2022-01-22 23:58:34 -0500

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+    <Head>
+      <title>Analogic | Gear made easy</title>
+      <meta
+        name="description"
+        content="something about the site here"
+      />
+    </Head>
+      <h1>Analogic</h1>
+    </div>
+  )
+}
